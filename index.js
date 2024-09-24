e1a967380741c06dd2d4bfe0b35059b13f2fd399
Nomu1Nomu1 <isaiah.tino@gmail.com>
2024-09-24 09:32:28 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import e from 'express';
+import cors from 'cors';
+import router from './routes/web.js';
+
+const app = e();
+const port = 3000;
+
+app.use(cors());
+app.use(e.json());
+app.use(router);
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port http://localhost:${port}`);
+})
