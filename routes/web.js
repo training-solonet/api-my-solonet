557a990bec3cb5bf9ac7ff86983ca68f432c71ec
Nomu1Nomu1 <isaiah.tino@gmail.com>
2024-09-26 09:32:03 +0700

    disable otp

--- routes/web.js
+++ routes/web.js
@@ -1,15 +1,18 @@
 import express from "express";
 import { product  } from '../controller/ProductController.js';
-import { login, register, getUser, verifyOtp, updateUser } from "../controller/UserController.js";
+import { login, register, getUser, updateUser } from "../controller/UserController.js";
 
 
 const router = express.Router();
 
+// User
 router.get("/users", getUser);
 router.post("/register", register);
 router.post("/login", login);
-router.get("/product", product);
-router.post("/verifyOtp", verifyOtp);
+// router.post("/verifyOtp", verifyOtp);
 router.put("/updateUser/:id", updateUser);
+
+// Product
+router.get("/product", product);
 
 export default router;
