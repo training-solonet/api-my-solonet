78e5a0cd879f2a98b0337a690879be9878e31ad9
Edy kurniawan <edy.kurniawan280898@gmail.com>
2024-09-26 11:05:45 +0700

    .env

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("my_solonet", "tino", "training2024", {
-  host: "connectis.my.id",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
 }); 
 
