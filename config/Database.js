3b3621a27ce92bb9f125b2f7443da30ef331c85f
Edy kurniawan <edy.kurniawan280898@gmail.com>
2024-09-26 11:32:57 +0700

    .env

--- config/Database.js
+++ config/Database.js
@@ -1,4 +1,6 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
