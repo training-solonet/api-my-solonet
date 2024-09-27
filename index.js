45622a2fef1e6c5ec38f129b0640e05051524a5e
asus <rasyadelgiva@gmail.com>
2024-09-27 11:05:07 +0700

    iya bang iya, nih ku kirim

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import e from "express";
+import session from "express-session"; 
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import cors from "cors";
 import router from "./routes/web.js";
 import dotenv from "dotenv";
@@ -10,10 +13,44 @@ const port = process.env.PORT;
 
 app.use(cors());
 app.use(e.json());
+
+app.use(session({
+  secret: process.env.SESSION_SECRET, 
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: 'http://localhost:5000/auth/google/callback'  
+}, (accessToken, refreshToken, profile, done) => {
+  return done(null, profile);  
+}));
+
 app.use(router);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/login", (req, res) => {
+  res.send("berhasil login");
+});
+
+app.get("/logout", (req, res) => {
+  res.send("berhasil logout");
 });
 
 app.listen(port, () => {
--- routes/web.js
+++ routes/web.js
@@ -1,10 +1,11 @@
 import express from "express";
+import passport from "passport";
+
 import { product, productById } from '../controller/ProductController.js';
 import { getPromo } from '../controller/PromoController.js';
 import { kategori }  from '../controller/kategoriController.js';
 import { login, register, getUser, updateUser } from "../controller/UserController.js";
 import { getPembelian } from "../controller/PembelianController.js";
-
 
 const router = express.Router();
 
@@ -23,5 +24,24 @@ router.get("/product/:id", productById );
 
 // Pembelian
 router.get("/pembelian", getPembelian);
+
+//google auth
+router.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+router.get('/auth/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+        res.redirect('/login');  
+    }
+);
+
+router.get('/logout', (req, res) => {
+    req.logout(() => {
+        res.redirect('/logout');
+    });
+});
+
 
 export default router;
