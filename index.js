8ac6348a645ee6ebe7ee7c893eb2b31878d49360
Elgiva Rasyad <141321900+Rasyaditya13@users.noreply.github.com>
2024-10-08 15:38:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ passport.use(
         "179217026619-dfhjpdk5njnoktot1hquafijbgkn2s3p.apps.googleusercontent.com",
       clientSecret:
         "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",
-      callbackURL: `https://api.connectis.my.id/auth/google`,
+      callbackURL: `https://api.connectis.my.id/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
