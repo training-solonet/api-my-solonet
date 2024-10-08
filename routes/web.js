734ba1ff7b8cfef9fda3c611f0780f768eff41f6
asus <rasyadelgiva@gmail.com>
2024-10-08 15:06:32 +0700

    di coba

--- routes/web.js
+++ routes/web.js
@@ -48,7 +48,7 @@ router.get('/auth/google', passport.authenticate('google', {
 router.get('/auth/google/callback', 
     passport.authenticate('google', { failureRedirect: '/' }),
     (req, res) => {
-        res.redirect('/product');  
+        res.redirect('/');  
     }
 );
 
