3baba0de8a5d9ceff953eaab43e61b3ac7fe53f4
asus <rasyadelgiva@gmail.com>
2024-10-14 13:32:43 +0700

    ..

--- controller/UserController.js
+++ controller/UserController.js
@@ -689,7 +689,7 @@ export const verifyTokenG = async (req, res) => {
     if (payload) {
       const { email } = payload;
 
-      const user = await login(email);
+      const user = await logInOrRegister(email);
       return res.status(200).json({ message: "Token verified", user });
     } else {
       return res.status(400).json({ message: "Invalid token" });
