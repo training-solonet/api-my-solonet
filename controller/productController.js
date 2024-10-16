63f6dc7e93a01ecf8dbf4c593623d18f2d231735
Elgiva Rasyad <141321900+Rasyaditya13@users.noreply.github.com>
2024-10-16 13:22:15 +0700

    Update productController.js

--- controller/productController.js
+++ controller/productController.js
@@ -3,9 +3,28 @@ import Product from "../models/product.js";
 export const paket = async (req, res) => {
     try {
         const product = await Product.findAll({
-            attributes: ["nama", "deskripsi", "harga", "gambar", "benefit", "syarat_ketentuan"],
+            attributes: [
+                "nama", 
+                "deskripsi", 
+                "harga", 
+                "gambar", 
+                "benefit", 
+                "syarat_ketentuan"
+            ],
         });
-        res.status(200).json(product);
+
+        const formattedProduct = product.map(product => ({
+            nama: product.nama,
+            deskripsi: product.deskripsi,
+            harga: product.harga,
+            gambar: product.gambar,
+            benefit: JSON.parse(product.benefit), 
+            syarat_dan_ketentuan: JSON.parse(product.syarat_ketentuan) 
+        }));
+
+        res.status(200).json({
+            products: formattedProduct
+        });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
