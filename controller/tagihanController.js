ce7f2fbee0e496acd6d5bb830367cbd8cfe3a6af
Nomu1Nomu1 <isaiah.tino@gmail.com>
2024-10-17 11:05:06 +0700

    cron schedule and get tagihan by user

--- controller/tagihanController.js
+++ controller/tagihanController.js
@@ -1,2 +1,29 @@
-import Tagihan from "../models/tagihan";
-import Customer from "../models/customer";
+import Tagihan from "../models/tagihan.js";
+import cron from "node-cron"
+
+export const tagihanUser = async (req, res) => {
+    const userId = req.user.id;
+
+    Tagihan.findByPk(userId)
+        .then((result) => {
+            res.json(result);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(500).json({ message: "Internal Server Error" });
+        });
+}
+
+cron.schedule("*/15 0 1 * *", async () => {
+    try {
+      await Tagihan.update(
+        { status_pembayaran: "0" }, 
+        {
+          where: {}, 
+        }
+      );
+      console.log("Cron job ran successfully: All Tagihan status updated.");
+    } catch (error) {
+      console.error("Error running cron job:", error);
+    }
+  });
