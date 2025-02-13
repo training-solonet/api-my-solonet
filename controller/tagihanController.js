044b2fc50960df1b6e2278414da91b99200e45f4
Edy kurniawan <edy.kurniawan280898@gmail.com>
2025-02-13 09:19:57 +0700

    delete cronjob tagihan

--- controller/tagihanController.js
+++ controller/tagihanController.js
@@ -1,6 +1,5 @@
 import Customer from "../models/customer.js";
 import Tagihan from "../models/tagihan.js";
-import cron from "node-cron"
 
 export const tagihanUser = async (req, res) => {
   try {
@@ -14,8 +13,8 @@ export const tagihanUser = async (req, res) => {
       const customerId = customers.map(customer => customer.id);
 
       const tagihan = await Tagihan.findAll({
-          where: { customer_id: customerId }
-        });
+        where: { customer_id: customerId }
+      });
 
       return res.status(200).json({ tagihan: tagihan });
   } catch (error) {
@@ -23,21 +22,3 @@ export const tagihanUser = async (req, res) => {
       return res.status(500).json({ message: 'Internal server error' });
   }
 };
-
-cron.schedule("*/15 0 1 * *", async () => {
-    const currentDate = new Date().toISOString().slice(0, 10);
-    try {
-      await Tagihan.update(
-        { 
-            status_pembayaran: "0",
-            bulan: currentDate
-         }, 
-        {
-          where: {}, 
-        }
-      );
-      console.log("Cron job ran successfully: All Tagihan status updated.");
-    } catch (error) {
-      console.error("Error running cron job:", error);
-    }
-  });
